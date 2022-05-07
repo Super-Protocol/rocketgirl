6f006b8301c37315de25dff5a279e90d125bf0d9
vyuv <kaavolv@gmail.com>
2022-05-07 10:41:50 +0300

    fix-proxy-request

--- src/apollo/links/wsLink.ts
+++ src/apollo/links/wsLink.ts
@@ -1,10 +1,12 @@
 import { WebSocketLink } from '@apollo/client/link/ws';
+import Cookies from 'js-cookie';
 import CONFIG from '@/config';
 // import { onLocalStorageEvent } from '@/utils/local-storage-events';
 
 const { host } = new URL(CONFIG.REACT_APP_API_ENDPOINT || window.location.href);
 const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
 const { REACT_APP_AUTH } = process.env;
+
 const wsLink = (): WebSocketLink => {
     // After changing account, closing this connection link because it'll be no more actual
     // onLocalStorageEvent('accountChanged', () => {
@@ -14,14 +16,22 @@ const wsLink = (): WebSocketLink => {
     return new WebSocketLink({
         uri: `${protocol}://${host}/graphql-subscriptions`,
         options: {
-            reconnect: true,
+            reconnect: false,
         },
         connectionParams: async () => {
-            return REACT_APP_AUTH ? {
+            const token = Cookies.get('id_token');
+            const account = Cookies.get('account');
+
+            const headers = REACT_APP_AUTH ? {
                 headers: {
                     authorization: REACT_APP_AUTH,
                 },
             } : {};
+            return {
+                token,
+                account,
+                ...headers,
+            };
         },
     });
 };
