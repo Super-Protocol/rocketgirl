8840a9f66545359be2940a1f504df320146eda9b
vyuv <kaavolv@gmail.com>
2022-05-09 11:45:47 +0300

    fix-proxy-request

--- src/apollo/links/wsLink.ts
+++ src/apollo/links/wsLink.ts
@@ -1,30 +1,16 @@
 import { WebSocketLink } from '@apollo/client/link/ws';
 import CONFIG from '@/config';
-// import { onLocalStorageEvent } from '@/utils/local-storage-events';
 
 const { host } = new URL(CONFIG.REACT_APP_API_ENDPOINT || window.location.href);
-const { REACT_APP_AUTH } = process.env;
+const { REACT_APP_BASIC_AUTH } = process.env;
+
+const basicAuth = REACT_APP_BASIC_AUTH ? `${REACT_APP_BASIC_AUTH}@` : '';
 
 const wsLink = (): WebSocketLink => {
-    // After changing account, closing this connection link because it'll be no more actual
-    // onLocalStorageEvent('accountChanged', () => {
-    //     wsConnectionLink.subscriptionClient.client.close();
-    // }, { once: true });
-
     return new WebSocketLink({
-        uri: `wss://${host}/graphql`,
+        uri: `wss://${basicAuth}${host}/graphql`,
         options: {
             reconnect: true,
-        },
-        connectionParams: async () => {
-            const headers = REACT_APP_AUTH ? {
-                headers: {
-                    authorization: REACT_APP_AUTH,
-                },
-            } : {};
-            return {
-                ...headers,
-            };
         },
     });
 };
