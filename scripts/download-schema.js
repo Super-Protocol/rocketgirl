758533486b1859200169686403583957c83740f3
vyuv <kaavolv@gmail.com>
2022-05-11 11:01:25 +0300

    fix download schema

--- scripts/download-schema.js
+++ scripts/download-schema.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
+require('dotenv').config();
 
 const { REACT_APP_API_ENDPOINT, REACT_APP_AUTH } = process.env;
-
-execSync(`npx get-graphql-schema ${REACT_APP_API_ENDPOINT} --header Authorization=${REACT_APP_AUTH} > schema.graphql`);
+execSync(`npx get-graphql-schema ${REACT_APP_API_ENDPOINT} --header 'Authorization=${REACT_APP_AUTH}' > schema.graphql`);
