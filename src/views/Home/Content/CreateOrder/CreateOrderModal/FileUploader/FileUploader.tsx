2fd37a4ccc7d236e24c8261728fda47b83f9da64
vyuv <kaavolv@gmail.com>
2022-06-01 15:11:37 +0300

    SP-1197

--- src/views/Home/Content/CreateOrder/CreateOrderModal/FileUploader/FileUploader.tsx
+++ src/views/Home/Content/CreateOrder/CreateOrderModal/FileUploader/FileUploader.tsx
@@ -1,15 +1,61 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { S3 } from 'aws-sdk';
 
 import { Box, Icon } from '@/uikit';
 import classes from './FileUploader.module.scss';
+import { S3_BUCKET, S3_CONFIG } from './helpers';
+
+const client = new S3(S3_CONFIG);
 
 export const FileUploader = (): ReactElement => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const [progress, setProgress] = useState(0);
+    const [uploadedFiles, setUploadedFiles] = useState<string[]>([]);
+
+    const uploadFile = useCallback(async (file) => {
+        const params = {
+            ACL: 'public-read',
+            Body: file,
+            Bucket: S3_BUCKET,
+            Key: file.name,
+        };
+        const idx = uploadedFiles.findIndex((el) => el === file.name);
+        if (idx >= 0) {
+            return;
+        }
+
+        client.putObject(params)
+            .on('httpUploadProgress', (evt) => {
+                setProgress(Math.round((evt.loaded / evt.total) * 100));
+            })
+            .on('complete', (_) => {
+                setUploadedFiles((prev) => [...prev, file.name]);
+            })
+            .send((err) => {
+                if (err) console.error(err);
+            });
+    }, [uploadedFiles]);
+
+    const {
+        getRootProps, getInputProps,
+    } = useDropzone({
+        onDrop: (acceptedFiles) => {
+            for (let i = 0; i < acceptedFiles.length; i++) {
+                uploadFile(acceptedFiles[i]);
+            }
+        },
+    });
+
     return (
         <Box direction="column">
             <div className={classes.title}>File</div>
-            <section className={classes.container}>
+            <Box direction="column">
+                <ul>
+                    {uploadedFiles.map((item) => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ul>
+                <div>{progress}</div>
                 <div {...getRootProps({ className: classes.dropzone })}>
                     <input {...getInputProps()} />
                     <Box alignItems="center">
@@ -17,7 +63,7 @@ export const FileUploader = (): ReactElement => {
                         <p>Add file or drag and drop</p>
                     </Box>
                 </div>
-            </section>
+            </Box>
             <div className={classes.errorEmpty} />
         </Box>
     );
---
+++ src/views/Home/Content/CreateOrder/CreateOrderModal/FileUploader/helpers.ts
@@ -0,0 +1,12 @@
+export const S3_BUCKET = 'inputs';
+
+export const S3_CONFIG = {
+    credentials: {
+        accessKeyId: 'jwkgg66m7l5tehzsxwe4jyjdx7ra',
+        secretAccessKey: 'jyg5zqgiin4ne4gb2qgnnljhtk2urji3xhslgmtchutbdsf5vfpas',
+    },
+    endpoint: 'https://gateway.storjshare.io',
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4',
+    httpOptions: { timeout: 0 },
+};
