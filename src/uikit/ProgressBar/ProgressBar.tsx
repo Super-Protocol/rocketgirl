afcfef14fb8f7a06066a451428c59f1bbcd4fb7f
vyuv <kaavolv@gmail.com>
2022-06-28 17:28:42 +0300

    SP-1529

--- src/uikit/ProgressBar/ProgressBar.tsx
+++ src/uikit/ProgressBar/ProgressBar.tsx
@@ -1,14 +1,18 @@
 import React, { memo, FC } from 'react';
+import cn from 'classnames';
+
 import { ProgressBarProps } from './types';
 import classes from './ProgressBar.module.scss';
+
+const valuemax = 100;
 
 export const ProgressBar: FC<ProgressBarProps> = memo(({ progress }) => {
     return (
         <div className={classes.progress}>
             <div
-                className={classes.progressBar}
+                className={cn(classes.progressBar, { [classes['progressBar-progress']]: progress !== valuemax })}
                 style={{ width: `${progress}%` }}
-                aria-valuemax={100}
+                aria-valuemax={valuemax}
                 aria-valuenow={progress}
                 aria-valuemin={0}
             />
