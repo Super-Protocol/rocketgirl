6513f8a1685ea798611706096e8cb852d46fb0c0
vyuv <kaavolv@gmail.com>
2022-06-30 14:42:23 +0300

    SP-1547

--- src/uikit/Checkbox/CheckboxUi.tsx
+++ src/uikit/Checkbox/CheckboxUi.tsx
@@ -6,7 +6,7 @@ import cn from 'classnames';
 import { v1 as uuidv1 } from 'uuid';
 
 import { ReactComponent as CheckmarkIcon } from '@/assets/icons/checkmark.svg';
-import { Tooltip } from '@/uikit';
+import { Tooltip, Box } from '@/uikit';
 import { calcLabelLimit } from './helpers';
 import { CheckboxUiProps, themes } from './types';
 import classes from './CheckboxUi.module.scss';
@@ -25,12 +25,15 @@ export const CheckboxUi: FC<CheckboxUiProps> = memo(({
     classNameWrap,
     classNameLabel,
     classNameCheckboxCheckmark,
+    classNameError,
     labelClickDisabled,
     dataTestId,
     theme = themes.orange,
     labelLimit,
     localState,
     withLabelTooltip = false,
+    showError,
+    error,
     ...rest
 }) => {
     const [nameFor] = useState(name || uuidv1());
@@ -62,59 +65,64 @@ export const CheckboxUi: FC<CheckboxUiProps> = memo(({
     }, [tooltip, withLabelTooltip, renderLabel]);
 
     return (
-        <label
-            className={cn(classes.checkbox, classNameWrap, { [classes.disabled]: disabled })}
-            htmlFor={nameFor}
-            data-testid={dataTestId}
-        >
-            <input
-                id={nameFor}
-                name={nameFor}
-                checked={checked}
-                type="checkbox"
-                disabled={disabled}
-                className={classes.checkbox__control}
-                onChange={(evt) => {
-                    if (!disabled) {
-                        onChange(evt.target.checked, evt);
-                    }
-                }}
-                {...rest}
-            />
-            <i className={cn(classes.checkbox__checkmark, classes[theme], classNameCheckboxCheckmark,
-                { [classes.checkbox__checkmark_invalid]: isInvalid },
-                { [classes.checkbox__checkmark_disabled]: disabled },
-                { [classes.checkbox__checkmark_semi]: semi })}
+        <Box direction="column">
+            <label
+                className={cn(classes.checkbox, classNameWrap, { [classes.disabled]: disabled })}
+                htmlFor={nameFor}
+                data-testid={dataTestId}
             >
-                <CheckmarkIcon />
-            </i>
-            <span
-                className={cn(classes.checkbox__label,
-                    classNameLabel,
-                    { [classes.checkbox__label_invalid]: isInvalid },
-                    { [classes.checkbox__label_disabled]: disabled })}
-                data-testid="checkbox-label"
-                onClick={handleOnClick}
-            >
-                {labelURL ? (
-                    <Link to={{ pathname: labelURL, state: { from: document.location.pathname, localState } }}>
-                        {label}
-                    </Link>
-                ) : (
-                    <>
-                        {
-                            renderTooltip && (
-                                <Tooltip tooltip={renderTooltip} classNamePopoverChildren={classes.labelTooltip}>
-                                    {renderLabel}
-                                </Tooltip>
-                            )
+                <input
+                    id={nameFor}
+                    name={nameFor}
+                    checked={checked}
+                    type="checkbox"
+                    disabled={disabled}
+                    className={classes.checkbox__control}
+                    onChange={(evt) => {
+                        if (!disabled) {
+                            onChange(evt.target.checked, evt);
                         }
-                        {
-                            !renderTooltip && renderLabel
-                        }
-                    </>
-                )}
-            </span>
-        </label>
+                    }}
+                    {...rest}
+                />
+                <i className={cn(classes.checkbox__checkmark, classes[theme], classNameCheckboxCheckmark,
+                    { [classes.checkbox__checkmark_invalid]: isInvalid },
+                    { [classes.checkbox__checkmark_disabled]: disabled },
+                    { [classes.checkbox__checkmark_semi]: semi })}
+                >
+                    <CheckmarkIcon />
+                </i>
+                <span
+                    className={cn(classes.checkbox__label,
+                        classNameLabel,
+                        { [classes.checkbox__label_invalid]: isInvalid },
+                        { [classes.checkbox__label_disabled]: disabled })}
+                    data-testid="checkbox-label"
+                    onClick={handleOnClick}
+                >
+                    {labelURL ? (
+                        <Link to={{ pathname: labelURL, state: { from: document.location.pathname, localState } }}>
+                            {label}
+                        </Link>
+                    ) : (
+                        <>
+                            {
+                                renderTooltip && (
+                                    <Tooltip tooltip={renderTooltip} classNamePopoverChildren={classes.labelTooltip}>
+                                        {renderLabel}
+                                    </Tooltip>
+                                )
+                            }
+                            {
+                                !renderTooltip && renderLabel
+                            }
+                        </>
+                    )}
+                </span>
+            </label>
+            {showError && isInvalid && error && (
+                <span data-testid="checkbox-error" className={cn(classes.error, classNameError)}>{error}</span>
+            )}
+        </Box>
     );
 });
