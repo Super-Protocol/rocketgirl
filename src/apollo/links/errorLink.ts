79c3451d3f69b3a685894eb0f16f5bf9c60b6a8c
galkinsden <galkinsden@gmail.com>
2022-07-29 10:27:30 +0300

    SP-1754

--- src/apollo/links/errorLink.ts
+++ src/apollo/links/errorLink.ts
@@ -2,8 +2,16 @@ import { onError } from '@apollo/client/link/error';
 import { ServerError } from '@apollo/client/link/utils/throwServerError';
 import { AUTH_TOKEN } from '@/common/constants';
 
-const errorLink = onError(({ networkError, operation }) => {
-    if ((networkError as ServerError)?.statusCode === 401 && operation?.operationName !== 'CheckAuthToken') {
+const errorLink = onError(({
+    networkError,
+    operation,
+    response,
+    graphQLErrors,
+}) => {
+    if (
+        ((networkError as ServerError)?.statusCode === 401 && operation?.operationName !== 'CheckAuthToken')
+        || graphQLErrors?.some(({ extensions }) => (extensions as any)?.response?.statusCode === 401)
+    ) {
         localStorage.setItem(AUTH_TOKEN, '');
         window.location.href = '/login';
     }
